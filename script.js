c01c289ebe4240cc8b654b8f5ed6fcbd6a81bf8f
crono628 <m.desantis628@gmail.com>
2021-08-17 22:17:04 -0400

    working

--- script.js
+++ script.js
@@ -3,27 +3,36 @@
 // const myPages = document.querySelector('.pages')
 // const myFinished = document.querySelector('.finished')
 // const collection = document.querySelector('.book-collection')
+
 const subBtn = document.querySelector('.submit-btn')
 
 let myLibrary = [];
 
-function Book() {
-    this.title = ''
-    this.author = ''
-    this.pages = ''
-    this.read = ''
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author 
+    this.pages = pages 
+    this.read = read
 }
 
-function addBookToLibrary() {
-    newBook = new Book()
+let newBook = new Book()
+
+function addBookToLibrary(title, author, pages, read) {
+    title = newBook.title
+    author = newBook.author
+    pages = newBook.pages
+    read = newBook.read
     newBook.title = form.title.value
     newBook.author = form.author.value
     newBook.pages = form.pages.value
     newBook.read = form.read.value
-    newBook.push(myLibrary)
-    console.log(myLibrary);
-    
 }
-console.log('test')
 
-subBtn.onclick = () => addBookToLibrary();
+console.log(addBookToLibrary(title, author, pages, read));
+
+subBtn.addEventListener('click', () => {
+    addBookToLibrary().push(myLibrary)
+    console.log(myLibrary);
+    console.log('testing');
+});
+
