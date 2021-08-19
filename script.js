2f3d72d1e8d3707acbf7b76be43ef25795f4cda2
crono628 <m.desantis628@gmail.com>
2021-08-18 21:15:30 -0400

    working

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-// const myTitle = document.querySelector('.title').value
-// const myAuthor = document.querySelector('.author').value
-// const myPages = document.querySelector('.pages').value
-// const myRead = document.querySelector('.read').checked
+const myTitle = document.querySelector('.new-title')
+const myAuthor = document.querySelector('.new-author')
+const myPages = document.querySelector('.new-pages')
+const myRead = document.querySelector('.new-read')
 const collection = document.querySelector('.book-collection')
-
 const subBtn = document.querySelector('#submit-btn')
 
+let books = 0
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
 
+function addBookToLibrary(newBook) {
+    let title = document.querySelector('.title').value
+    let author = document.querySelector('.author').value
+    let pages = document.querySelector('.pages').value
+    let read = document.querySelector('.read').checked
 
-function addBookToLibrary() {
-  title = document.querySelector('.title').value
-  author = document.querySelector('.author').value
-  pages = document.querySelector('.pages').value
-  read = document.querySelector('.read').checked
-
-  let newBook = new Book(title, author, pages, read)
-
-  myLibrary.push(newBook)
-  console.log(newBook)
+    newBook = new Book(title, author, pages, read)
+    myLibrary.unshift(newBook)
+    books++
 }
 
+function identifyBook (){
+    myTitle.textContent= myLibrary[0].title
+    myAuthor.textContent=myLibrary[0].author
+    myPages.textContent=myLibrary[0].pages
+    myRead.textContent=myLibrary[0].pages
+}
 
 subBtn.addEventListener('click', function () {
-  addBookToLibrary()
-  console.log(myLibrary)
+    addBookToLibrary()
+    identifyBook()
+    console.log(myLibrary)
+    console.log(books);
 })
+
+// first, make sure there is a div that will be the container for all books on the bookshelf
+
+// make identifyBook() create a div (will be the 'book') and create <p> inside the div to house text content for title, author, pages, read 
+
+// the div book can be a set height and width. the container can have padding or whatever
