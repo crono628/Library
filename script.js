796cb9df45f402be11ccdc74e6c870ac0aac525c
crono628 <84817352+crono628@users.noreply.github.com>
2021-08-20 20:28:59 +0000

    functioning and a little better looking. added delete button and popup book entry

--- script.js
+++ script.js
@@ -4,84 +4,116 @@ const myPages = document.querySelector('.pages')
 const myRead = document.querySelector('.read')
 const collection = document.querySelector('.book-collection')
 const subBtn = document.querySelector('#submit-btn')
-
-let books = 0
+const addBtn = document.querySelector('.add-btn')
+const popup = document.querySelector('#my-popup')
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
 }
 
 function addBookToLibrary(newBook) {
-    let title = document.querySelector('.title').value
-    let author = document.querySelector('.author').value
-    let pages = document.querySelector('.pages').value
-    let read = document.querySelector('.read').checked
+  let title = document.querySelector('.title').value
+  let author = document.querySelector('.author').value
+  let pages = document.querySelector('.pages').value
+  let read = document.querySelector('.read').checked
 
-    newBook = new Book(title, author, pages, read)
-    myLibrary.unshift(newBook)
-    books++
+  newBook = new Book(title, author, pages, read)
+  myLibrary.unshift(newBook)
 }
 
 function createBook() {
-    const newDiv = document.createElement('div')
-    newDiv.setAttribute('class', 'new-book')
-    newDiv.style.border = '5px solid black'
-    newDiv.style.backgroundColor = 'brown'
-    newDiv.style.margin = '5px'
-    newDiv.style.height = '175px'
-    newDiv.style.width = '125px'
-    // newDiv.style.
-    // newDiv.style.
-    const newTitle = document.createElement('p')
-    newTitle.setAttribute('class', 'new-title')
-    const newAuthor = document.createElement('p')
-    newAuthor.setAttribute('class', 'new-author')
-    const newPages = document.createElement('p')
-    newPages.setAttribute('class', 'new-pages')
-    const newRead = document.createElement('p')
-    newRead.setAttribute('class', 'new-read')
+  const newDiv = document.createElement('div')
+  newDiv.setAttribute('class', 'new-book')
+  newDiv.style.border = '5px solid black'
+  newDiv.style.backgroundColor = 'brown'
+  newDiv.style.margin = '5px'
+  newDiv.style.height = '200px'
+  newDiv.style.width = '150px'
+  newDiv.style.color = 'white'
+  // newDiv.style.
+  const newTitle = document.createElement('p')
+  newTitle.setAttribute('class', 'new-title')
+  const newAuthor = document.createElement('p')
+  newAuthor.setAttribute('class', 'new-author')
+  const newPages = document.createElement('p')
+  newPages.setAttribute('class', 'new-pages')
+  const newRead = document.createElement('p')
+  newRead.setAttribute('class', 'new-read')
+  const newDeleteBtn = document.createElement('button')
+  newDeleteBtn.setAttribute('id', 'delete-btn')
+  newDeleteBtn.textContent = 'X'
 
-    collection.appendChild(newDiv)
-    newDiv.appendChild(newTitle)
-    newDiv.appendChild(newAuthor)
-    newDiv.appendChild(newPages)
-    newDiv.appendChild(newRead)
+  collection.appendChild(newDiv)
+  newDiv.appendChild(newTitle)
+  newDiv.appendChild(newAuthor)
+  newDiv.appendChild(newPages)
+  newDiv.appendChild(newRead)
+  newDiv.appendChild(newDeleteBtn)
 
-    newTitle.textContent = myLibrary[0].title
-    newAuthor.textContent = myLibrary[0].author
-    newPages.textContent = myLibrary[0].pages
+  newDeleteBtn.addEventListener('click', function () {
+    deleteBook(myLibrary, collection.removeChild(newDiv))
+  })
 
-    if (myLibrary[0].read == true) {
-        newRead.textContent = 'Book is read'
-    } else {
-        newRead.textContent = 'Book is unread'
-    }
+  newTitle.textContent = myLibrary[0].title
+  newAuthor.textContent = myLibrary[0].author
+  newPages.textContent = myLibrary[0].pages
+
+  if (myLibrary[0].title == '') {
+    newTitle.textContent = 'Unknown Title'
+  }
+
+  if (myLibrary[0].author === '') {
+    newAuthor.textContent = 'Unknown Author'
+  }
+
+  if (myLibrary[0].pages === '') {
+    newPages.textContent = 'Unknown Pages'
+  }
+
+  if (myLibrary[0].read == true) {
+    newRead.textContent = 'Book is read'
+  } else {
+    newRead.textContent = 'Book is unread'
+  }
 }
 
 const clearInput = () => {
-    myTitle.value = ''
-    myAuthor.value = ''
-    myPages.value = ''
-    myRead.checked = false
+  myTitle.value = ''
+  myAuthor.value = ''
+  myPages.value = ''
+  myRead.checked = false
 }
 
-subBtn.addEventListener('click', function () {
-    if ((myTitle.value == '') || (myAuthor.value == '')) {
-        alert('Please enter a title and/or an author')
-    } else {
-        addBookToLibrary()
-        createBook()
-        clearInput()
-        console.log(books);
-    }
+function openForm() {
+  popup.style.display = "block";
+}
+
+function closeForm() {
+  popup.style.display = "none";
+}
+
+addBtn.addEventListener('click', function () {
+openForm()
 })
 
-// first, make sure there is a div that will be the container for all books on the bookshelf
+subBtn.addEventListener('click', function () {
+  if ((!myTitle.value.replace(/\s/g, '').length) || (!myAuthor.value.replace(/\s/g, '').length)) {
+    console.log('Please enter a title and/or author')
+  } else {
+    addBookToLibrary()
+    createBook()
+    clearInput()
+    closeForm()
+  }
+})
 
-// make createBook() create a div (will be the 'book') and create <p> inside the div to house text content for title, author, pages, read 
-
-// the div book can be a set height and width. the container can have padding or whatever
+function deleteBook(arr, book) {
+  if (arr.indexOf(book) >= 0) {
+    arr.splice(arr.indexOf(book), 1)
+  }
+  return arr
+}
