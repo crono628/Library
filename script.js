1dd22ee6572c2c21ccacd1fbadb54ad48b9bf35d
Michael DeSantis <m.desantis628@gmail.com>
2021-09-29 22:08:32 -0400

    css in process. html mostly done. starting js

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ const addButton = document.querySelector('.add-book-btn')
 const bookShelf = document.querySelector('.book-shelf')
 const container = document.querySelector('.container')
 const popupContainer = document.querySelector('.popup-container')
+const popupBtn = document.querySelectorAll('.popup-btn')
 
 addButton.addEventListener('click', () => {
   let newDiv = document.createElement('div')
   container.appendChild(newDiv)
   newDiv.classList.add('book')
   openForm()
+})
+
+popupBtn.forEach(button => {
+  button.addEventListener('click', () => {
+    let btnData = button.dataset.popup
+    if (btnData == 'submit') {
+      closeForm()
+    }
+    if (btnData == 'cancel') {
+      closeForm()
+    }
+  })
 })
 
 function openForm() {
