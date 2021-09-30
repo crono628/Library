8bf3ca4620b8dd4af3d6de0b21340d24444c8459
crono628 <84817352+crono628@users.noreply.github.com>
2021-09-30 19:07:44 +0000

    functioning: add book without details, css animations. js in process

--- script.js
+++ script.js
@@ -3,30 +3,56 @@ const bookShelf = document.querySelector('.book-shelf')
 const container = document.querySelector('.container')
 const popupContainer = document.querySelector('.popup-container')
 const popupBtn = document.querySelectorAll('.popup-btn')
+let body = document.querySelector('.body-container')
+
+class Book {
+  constructor(title, author, pages, status) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = status
+  }
+
+  createBookDiv() {
+    let newDiv = document.createElement('div')
+    let newTitle = document.createElement('h2')
+    newDiv.classList.add('book')
+    bookShelf.appendChild(newDiv)
+    newDiv.appendChild(newTitle)
+  }
+
+}
 
 addButton.addEventListener('click', () => {
-  let newDiv = document.createElement('div')
-  container.appendChild(newDiv)
-  newDiv.classList.add('book')
-  openForm()
+  body.classList.replace('blur-out', 'blur')
+  popupContainer.style.display = 'flex'
+  popupContainer.classList.remove('popup-fadeout')
+  popupContainer.classList.add('popup-fadein')
 })
 
 popupBtn.forEach(button => {
   button.addEventListener('click', () => {
     let btnData = button.dataset.popup
     if (btnData == 'submit') {
-      closeForm()
+      let book = new Book
+      book.createBookDiv()
+      popupLeaving()
+      setTimeout(closeForm, 600)
     }
     if (btnData == 'cancel') {
-      closeForm()
+      popupLeaving()
+      setTimeout(closeForm, 600)
     }
   })
 })
 
-function openForm() {
-  popupContainer.style.display = 'flex'
+function popupLeaving() {
+  body.classList.replace('blur', 'blur-out')
+  popupContainer.classList.remove('popup-fadein')
+  popupContainer.classList.add('popup-fadeout')
 }
 
 function closeForm() {
   popupContainer.style.display = 'none'
 }
+
