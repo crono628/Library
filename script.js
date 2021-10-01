f9a12384d07eba334066cfa864de0071990d0479
Michael DeSantis <m.desantis628@gmail.com>
2021-09-30 21:35:58 -0400

    all functioning except book buttons

--- script.js
+++ script.js
@@ -12,21 +12,19 @@ class Book {
     this.pages = pages
     this.status = status
   }
-
-  createBookDiv() {
-    let newBook = document.createElement('div')
-    let newTitle = document.createElement('h2')
-    let newAuthor = document.createElement('p')
-    let newPages = document.createElement('p')
-    let newStatus = document.createElement('p')
-    newBook.classList.add('book')
-    bookShelf.appendChild(newBook)
-    newBook.appendChild(newTitle)
-    newBook.appendChild(newAuthor)
-    newBook.appendChild(newPages)
-    newBook.appendChild(newStatus)
-  }
 }
+
+bookCollection = [{
+  title: "tester",
+  author: "",
+  pages: "100",
+  status: "Completed"
+}, {
+  title: "test",
+  author: "",
+  pages: "200",
+  status: "Completed"
+}]
 
 addButton.addEventListener('click', () => {
   body.classList.replace('blur-out', 'blur')
@@ -39,17 +37,58 @@ popupBtn.forEach(button => {
   button.addEventListener('click', () => {
     let btnData = button.dataset.popup
     if (btnData == 'submit') {
-      let book = new Book
-      book.createBookDiv()
+      addBook()
       popupLeaving()
       setTimeout(closeForm, 600)
+      clearForm()
+      purgeLibrary()
+      refresh(bookCollection)
     }
     if (btnData == 'cancel') {
       popupLeaving()
       setTimeout(closeForm, 600)
+      clearForm()
     }
   })
 })
+
+function createBookDiv(bookObj) {
+  let bookIndex = getIndex(bookObj)
+  let newBook = document.createElement('div')
+  let newTitle = document.createElement('h2')
+  let newAuthor = document.createElement('p')
+  let newPages = document.createElement('p')
+  let newStatus = document.createElement('p')
+  newBook.classList.add('book')
+  newBook.dataset.index = bookIndex
+  newTitle.classList.add('new-title')
+  newAuthor.classList.add('new-author')
+  newPages.classList.add('new-pages')
+  newStatus.classList.add('new-status')
+  newTitle.innerText = bookObj.title
+  newAuthor.innerText = bookObj.author
+  newPages.innerText = bookObj.pages
+  newStatus.innerText = bookObj.status
+  bookShelf.appendChild(newBook)
+  newBook.appendChild(newTitle)
+  newBook.appendChild(newAuthor)
+  newBook.appendChild(newPages)
+  newBook.appendChild(newStatus)
+
+  const newDeleteBtn = document.createElement("button");
+  newDeleteBtn.setAttribute("id", "delete-btn");
+  newDeleteBtn.textContent = "Remove Book";
+  // newDeleteBtn.addEventListener("click", deleteBook);
+  newDeleteBtn.dataset.index = bookIndex;
+  newBook.appendChild(newDeleteBtn);
+
+  const changeRead = document.createElement("button");
+  changeRead.setAttribute("id", "switch");
+  changeRead.textContent = "Change Status";
+  changeRead.dataset.index = bookIndex;
+  // changeRead.addEventListener("click", toggleRead);
+  newBook.appendChild(changeRead);
+}
 
 function popupLeaving() {
   body.classList.replace('blur', 'blur-out')
@@ -61,3 +100,38 @@ function closeForm() {
   popupContainer.style.display = 'none'
 }
 
+function addBook() {
+  let newBook = new Book(
+    document.querySelector('#title').value,
+    document.querySelector('#author').value,
+    document.querySelector('#pages').value,
+    document.querySelector('input[name="book-choice"]:checked').value
+  )
+  bookCollection.push(newBook)
+  createBookDiv(newBook)
+}
+
+function purgeLibrary() {
+  while (bookShelf.firstChild) {
+    bookShelf.removeChild(bookShelf.firstChild)
+  }
+}
+
+function clearForm() {
+  document.getElementsByName('book-choice').checked = false
+  document.querySelector('#title').value = ''
+  document.querySelector('#author').value = ''
+  document.querySelector('#pages').value = ''
+}
+
+function getIndex(thing) {
+  return bookCollection.indexOf(thing)
+}
+
+function refresh(library) {
+  library.forEach((book) => {
+    createBookDiv(book)
+  })
+}
+
+refresh(bookCollection)
